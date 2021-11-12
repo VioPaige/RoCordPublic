3b39c27fc9b302c4fd1e3f1fbe6a1d273adf89c0
VioPaige <85783108+VioPaige@users.noreply.github.com>
2021-11-12 19:57:40 +0100

    Create channelbutton.lua

---
+++ channelbutton.lua
@@ -0,0 +1,5 @@
+-- credit to https://github.com/VioPaige
+
+script.Parent.MouseButton1Click:Connect(function()
+	game.Players.LocalPlayer.PlayerGui.Rocord.ActiveChannel.Value = script.Parent.Id.Value
+end)
