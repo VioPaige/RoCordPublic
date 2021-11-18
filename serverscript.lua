ba04e123f06d77939265a5ac71aeefbd3d0f7164
VioPaige <85783108+VioPaige@users.noreply.github.com>
2021-11-18 21:15:29 +0100

    Update serverscript.lua

--- serverscript.lua
+++ serverscript.lua
@@ -8,42 +8,49 @@ local rStorage = game:GetService("ReplicatedStorage")
 
 
 -- Instances
-    -- Folders
+	-- Folders
 local remoteFolder = rStorage:WaitForChild("Remotes")
-    -- Other
-local dataRemote = remoteFolder:WaitForChild("GetData")
-
+	-- Other
+local getRemote = remoteFolder:WaitForChild("GetData")
+local postRemote = remoteFolder:WaitForChild("PostData")
 
 
 -- Variables
-local api = "https://example.com" -- replace with own domain
+local api = "https://rocordbackend.herokuapp.com"
 
 
 
 
 -- Functions
 function getGuild()
-    local res = http:GetAsync(api ..'/get/guild')
-    res = http:JSONDecode(res)
-    return res
+	local res = http:GetAsync(api ..'/get/guild')
+	res = http:JSONDecode(res)
+	return res
 end
 
 function getChannel(id)
-    local res = http:GetAsync(api ..'/get/channel/'.. id)
-    res = http:JSONDecode(res)
-    return res
+	local res = http:GetAsync(api ..'/get/channel/'.. id)
+	res = http:JSONDecode(res)
+	return res
 end
 
 function getMessages(channelid)
-    local res = http:GetAsync(api ..'/get/channelmessages/'.. channelid)
-    res = http:JSONDecode(res)
-    return res
+	local res = http:GetAsync(api ..'/get/channelmessages/'.. channelid)
+	res = http:JSONDecode(res)
+	return res
 end
 
 function getAuthor(channelid, messageid)
-    local res = http:GetAsync(api ..'/get/messageauthor/'.. channelid ..'/'.. messageid)
-    res = http:JSONDecode(res)
-    return res
+	local res = http:GetAsync(api ..'/get/messageauthor/'.. channelid ..'/'.. messageid)
+	res = http:JSONDecode(res)
+	return res
+end
+
+
+function postMessage(user, content, channelid)
+	local res = http:GetAsync(api ..'/post/message/'.. user ..'/'.. channelid ..'/'.. content)
+	--res = http:JSONDecode(res)
+	return res
 end
 
 
@@ -52,25 +59,38 @@ end
 
 
 
-function dataRemote.OnServerInvoke(plr, reqtype, id, id2)
-    print(plr, reqtype, id)    
-    local result = nil
-    
-    if reqtype == "guild" then
-        local res = getGuild()
-        result = res
-    elseif reqtype == "channel" then
-        local res = getChannel(id)
-        result = res
-    elseif reqtype == "messages" then
-        local res = getMessages(id)
-        for i, v in pairs(res) do
-            local author = getAuthor(v["channelId"], v["id"])
-            v["author"] = author
-        end
-        result = res        
-    end
-    return result
+function getRemote.OnServerInvoke(plr, reqtype, id, id2)
+	--print(plr, reqtype, id)	
+	local result = nil
+	
+	if reqtype == "guild" then
+		local res = getGuild()
+		result = res
+	elseif reqtype == "channel" then
+		local res = getChannel(id)
+		result = res
+	elseif reqtype == "messages" then
+		local res = getMessages(id)
+		for i, v in pairs(res) do
+			local author = getAuthor(v["channelId"], v["id"])
+			v["author"] = author
+		end
+		result = res		
+	end
+	--print("success")
+	return result
+end
+
+
+function postRemote.OnServerInvoke(plr, message, channelid)
+	print(plr, message)
+	local result = nil
+	
+	
+	result = postMessage(plr.Name, message, channelid)
+	
+	
+	return result
 end
 
 
